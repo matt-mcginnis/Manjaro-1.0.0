b1c7d477e341e15b3abfeb4a5d9fc47bee05b1d9
Matt McGinnis <mcginnisma90@gmail.com>
2022-03-24 19:34:59 -0400

    Adding oh-my-zsh installation.

---
+++ 200-Open-Source-Package-Installation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+############################################
+########## oh-my-zsh Installation ##########
+############################################
+
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
