a1164cd3bdd4563f3c04ec22aad5940d575535f0
Matt McGinnis <mcginnisma90@gmail.com>
2022-04-06 09:26:55 -0400

    Updating configuration file setup.

--- 300-Configuration-File-Setup.sh
+++ 300-Configuration-File-Setup.sh
@@ -4,22 +4,6 @@
 ########### Configuration Setup Script ###########
 ##################################################
 
-################################
-##### .config file changes #####
-################################
-
-# Replace i3 configuration with personal configuration
-#cp -r ~/GitHub/Manjaro/.config/i3/ ~/.config/
-
-# Replace i3blocks with personal configuration
-#cp -r ~/GitHub/Manjaro/.config/i3blocks/ ~/.config/
-
-# Copy neomuttrc personal configuration
-#cp -r ~/GitHub/Manjaro/.config/neomutt/ ~/.config/
-
-# Copy ranger personal configuration
-#cp -r ~/GitHub/Manjaro/.config/ranger/ ~/.config/
-
 ############################
 ##### etc file changes #####
 ############################
@@ -28,30 +12,12 @@
 #sudo rm /etc/default/tlp
 #sudo cp ~/GitHub/Manjaro/etc/default/tlp /etc/default/
 
-# Replace xorg.conf.d files
-#sudo rm -r /etc/X11/xorg.conf.d/
-#sudo cp -r ~/GitHub/Manjaro/etc/X11/xorg.conf.d/ /etc/X11/
-
 #############################
 ##### home file changes #####
 ############################a
 
 # Add .vimrc to ~/
 #cp ~/Github/Manjaro-1.0.0/home/.vimrc ~/
-
-# Add .Xmodmap to ~/
-#cp ~/GitHub/Manjaro/home/.Xmodmap ~/
-
-# Add .Xresources to ~/
-cp ~/Github/Manjaro-1.0.0/home/.Xresources ~/
-
-# Add .XresourcesHiDPI to ~/
-#cp ~/GitHub/Manjaro/home/.XresourcesHiDPI ~/
-
-# Add .Xstart.zsh to ~/
-# Be sure to add this as a startup application in 
-# order to load .Xresources in the Cinnamon Desktop.
-#cp ~/GitHub/Manjaro/home/.Xstart.zsh ~/
 
 #####################
 ##### zsh setup #####
@@ -73,12 +39,6 @@ git clone https://github.com/softmoth/zsh-vim-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/cu
 ###################################
 ##### Various System Settings #####
 ###################################
-
-# Run to use the locate command
-#sudo ionice -c3 updatedb
-
-# Set syncthing to run on startup
-#sudo systemctl enable syncthing@matt.service
 
 # Set time to local-time (Dual Boot with Windows)
 sudo timedatectl set-local-rtc 1 --adjust-system-clock
