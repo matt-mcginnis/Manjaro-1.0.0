1e5903317da0db9fe9460e915784bcf80f2f0dea
Matt McGinnis <mcginnisma90@gmail.com>
2022-05-26 14:40:20 -0400

    Added main package installation script

---
+++ MainPackageInstallation.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+########################################################
+########### Installation of Manjaro Packages ###########
+########################################################
+
+# It is important to run sudo pacman -Syu and 
+# reboot after a fresh installation. This script 
+# is intended to be run directly after a fresh 
+# installation and reboot.
+
+#########################################
+##### Loading Mirrors and Updating  #####
+#########################################
+
+# Replace pacman.conf with personal configuration
+sudo rm -R /var/lib/pacman/sync
+sudo cp etc/pacman.conf /etc/
+
+# Create mirror list
+sudo pacman-mirrors --geoip
+
+# Update repositories and packages
+sudo pacman -Syyu
+
+#######################################
+##### Adding Git SSH Key and Settings #####
+#######################################
+
+ssh-keygen -t ed25519 -C "mcginnisma90@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
+# Will need to cat ~/.ssh/id_ed25519.pub and add it to Github
+
+git config --global user.email "mcginnisma90@gmail.com"
+git config --global user.name "Matt McGinnis"
+git config --global core.editor "nvim"
+
+################################
+##### Package Installation #####
+################################
+
+# Install Docker
+sudo pacman -S docker
+
+# Install gvim
+sudo pacman -S neovim
+
+# Install kio-gdrive (Google Drive)
+sudo pacman -S kio-gdrive
+
+# Install pywal
+sudo pip3 install pywal
+
+# Install nodejs and npm
+sudo pacman -S nodejs npm
+
+# Install pyenv
+sudo pacman -S pyenv
+
+# Install qutebrowser
+sudo pacman -S qutebrowser
+
+# Install texlive-most
+sudo pacman -S texlive-most
+
+# Install unzip
+sudo pacman -S unzip
+
+#############################
+##### Install Oh-My-Zsh #####
+#############################
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#######################
+##### Install yay #####
+#######################
+
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R matt:legion ./yay-git
+cd yay-git
+sudo makepkg -si
+cd ..
+sudo rm -rf yay-git
