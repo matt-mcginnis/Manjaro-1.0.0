b57adab1103759c5ae4cfb75ee74d260883baaa8
Matt McGinnis <mcginnisma90@gmail.com>
2022-05-26 23:34:36 -0400

    Removing unnecessary scripts and config files

--- 200-Open-Source-Package-Installation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-############################################
-########## oh-my-zsh Installation ##########
-############################################
-
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
